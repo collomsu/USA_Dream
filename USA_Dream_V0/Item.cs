b99b644f1e07f14923fa2f814a0d7032f07548b5
YannickLeCam <149806445+YannickLeCam@users.noreply.github.com>
2025-05-05 15:36:11 +0200

    add(Class Item) , Set up temp V0

---
+++ USA_Dream_V0/Item.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace USA_Dream_V0
+{
+    public class Item
+    {
+        private string _name;
+        private string _description;
+        private decimal _price;
+        private Action _action;
+
+        public Item(string name, string description, decimal price,Action action)
+        {
+            this._name = name;
+            this._description = description;
+            this._price = price;
+            this._action = action;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+        }
+        public string Description
+        {
+            get { return _description; }
+        }
+        public decimal Price
+        {
+            get { return _price; }
+        }
+        public Action Action
+        {
+            get { return _action; }
+        }
+
+
+    }
+}
