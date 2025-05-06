6292aeb9271c582e7710c76e1cbe6e6933e907e4
Darren <stephandarren4@gmail.com>
2025-05-06 10:31:44 +0200

    ajout de fonction et constructeur dans human

--- USA_Dream_V0/Human.cs
+++ USA_Dream_V0/Human.cs
@@ -17,11 +17,62 @@ namespace USA_Dream_V0
             _name = name;
             _inventory = new Inventory(0, new List<Item>());
             _statistics = new List<Statistic>();
-            //TODO voir comment on definit les stats/items de base
         }
+
+        public Human(string name, Inventory inventory) : this(name)
+        {
+            _inventory = inventory;
+        }
+
+        public Human(string name, List<Statistic> statistics) : this(name)
+        {
+            _statistics = statistics;
+        }
+
+        public Human(string name, Inventory inventory, List<Statistic> statistics) : this(name, inventory)
+        {
+            _statistics = statistics;
+        }
+
 
         public string Name { get { return _name; } }
         public Inventory Inventory { get { return _inventory; } }
         public List<Statistic> Statistics { get {  return _statistics; } }
+
+        public void AddStatistic(Statistic statistic) 
+        { 
+            _statistics.Add(statistic); 
+        }
+
+        public bool ModifyStatistic(string name, int value) 
+        { 
+            Statistic? statToModify= _statistics.Find(s => s.Name == name);
+            if (statToModify == null) return false;
+            statToModify.Value += value;
+            return true;
+        }
+
+        public bool RemoveStatistic(string name) 
+        {
+            Statistic? statToDelete = _statistics.Find(s => s.Name == name);
+            if (statToDelete == null) return false;
+            _statistics.Remove(statToDelete);
+            return true;
+        }
+
+        public void AddItem(Item item) 
+        { 
+            _inventory.AddItem(item);
+        }
+
+        public bool RemoveItem(string name) 
+        { 
+            return _inventory.RemoveItem(name);
+        }
+
+        public void ModifyMoney(decimal value)
+        {
+            _inventory.Money += value;
+        }
     }
 }
